521763d11cbb456d3bd9c81e5484a5e42079a7d6
Md Rashedul Islam <rajib5570@gmail.com>
2025-05-19 20:26:03 +0600

    update skill controller added

--- src/app/modules/skills/skills.controller.ts
+++ src/app/modules/skills/skills.controller.ts
@@ -38,4 +38,37 @@ export class SkillControllers {
         sendResponse(res,200,true,"Skills fetched successfully",undefined,result)
     })
 
+    static getSingleSkill = catchAsync(async (req, res) => {
+        const result = await SkillServices.getSingleSkill(req.params.id);
+        sendResponse(res,200,true,"Skill fetched successfully",undefined,result)    
+    })
+
+    static updateSkill = catchAsync(async (req, res) => {
+        const payload = req.body;
+       
+           // Check if new image files are uploaded
+           if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+             const imageUrls = await Promise.all(
+               req.files.map(async (file) => {
+                 const uniqueSuffix = Date.now() + Math.round(Math.random() * 1e3);
+                 const imageName = `${uniqueSuffix}-${"rashedul-islam-rajib"}`;
+                 const path = file?.buffer;
+       
+                 const { secure_url } = await sendImageToCloudinary(imageName, path);
+                 return secure_url;
+               })
+             );
+             payload.icon = imageUrls;
+           } else {
+             const existingSkill = await SkillServices.getSingleSkill(
+               req.params.id
+             );
+             if (existingSkill) {
+               payload.icon = existingSkill.icon;
+             }
+        }
+        const result = await SkillServices.updateSkill(req.params.id, payload);
+        sendResponse(res,200,true,"Skill updated successfully",undefined,result)
+    })
+
 }
