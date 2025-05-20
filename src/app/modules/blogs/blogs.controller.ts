ed07352fbf0cd50ab53e60993e139abdcbb7d9bb
Md Rashedul Islam <rajib5570@gmail.com>
2025-05-20 11:14:29 +0600

    update single blogs controller added

--- src/app/modules/blogs/blogs.controller.ts
+++ src/app/modules/blogs/blogs.controller.ts
@@ -44,4 +44,33 @@ export class BlogControllers {
     sendResponse(res,200,true,"Blog fetched successfully",undefined,result)
   })
 
+  static updateBlog = catchAsync(async (req, res) => {
+     const payload = req.body;
+
+     // Check if new image files are uploaded
+     if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+       const imageUrls = await Promise.all(
+         req.files.map(async (file) => {
+           const uniqueSuffix = Date.now() + Math.round(Math.random() * 1e3);
+           const imageName = `${uniqueSuffix}-${"rashedul-islam-rajib"}`;
+           const path = file?.buffer;
+
+           const { secure_url } = await sendImageToCloudinary(imageName, path);
+           return secure_url;
+         })
+       );
+       payload.coverImage = imageUrls;
+     } else {
+       const existingBlog = await BlogServices.getSingleBlog(req.params.id);
+       if (existingBlog) {
+         payload.coverImage = existingBlog.coverImage;
+       }
+     }
+
+     const result = await BlogServices.updateBlog(req.params.id, payload);
+     sendResponse(res,200,true, "Blog updated successfully",undefined,result)
+    
+  })
+
+
 }
